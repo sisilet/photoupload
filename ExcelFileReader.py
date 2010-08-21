b31f31cdf08fa2fd733465be7e33a7207b720abd
ERIC <you@example.com>
2010-08-21 01:07:09 +0100

    .

--- ExcelFileReader.py
+++ ExcelFileReader.py
@@ -3,99 +3,140 @@
 
 import xlrd
 
-class ExcelFileReader:
-	def __init__(self):
-		self.filename = ""
-		self.wb = None
-		self.datasheet = -1
-		self.datarow = -1
-		self.datacol = -1
-		self.header = True
+class ExcelSheet:
+	class SheetIterator:
+		def __init__(self,sheet,r1,c1,header):
+			self.r1 = r1
+			self.c1 = c1
+			self.curr_record = 0
+			self.sheet = sheet
+			if header==True:
+				self.header = 1
+
+		def __first_record(self):
+			return self.r1 + self.header
+
+		def __nrecords(self):
+			return self.sheet.nrows - self.r1 - self.header
+
+		def next(self):
+			if self.__nrecords() > self.curr_record:
+				ret = self.sheet.row_values(self.curr_record+self.__first_record(),self.c1)
+			else:
+				raise StopIteration
+
+			self.curr_record = self.curr_record + 1
+			return ret
+
+	def __init__(self,s):
+		self.sheet = s
+		self.Header(True)
 		self.curr_record = 0
-	
+		if self.setPivotTopLeft() == False:
+			raise Exception
+		else:
+			pass
+
 	def __iter__(self):
-		return self
+		return ExcelSheet.SheetIterator(self.sheet,self.r1,self.c1,self.header)
 
 	def __len__(self):
-		ret = self.wb.sheet_by_index(self.datasheet).nrows - self.datarow
+		ret = self.sheet.nrows - self.r1
 		if self.header:
 			return ret - 1
 		else:
 			return ret
 
-	def load(self, filename, header = True, keyword = ''):
-		self.wb = xlrd.open_workbook(filename) # book class
-		
-		if(self.wb != None):
-			self.filename = filename
-
-		for si in range(self.wb.nsheets):
-			s = self.wb.sheet_by_index(si)
-			for r in range(s.nrows):
-				for c in range(s.ncols):
-					if keyword != '':
-						if s.cell(r,c) == keyword:
-							self.datasheet = si
-							self.datarow = r
-							self.datacol = c
-							return
-					else:
-						if s.cell(r,c).ctype != xlrd.XL_CELL_EMPTY:
-							self.datasheet = si
-							self.datarow = r
-							self.datacol = c
-							return
-
-#		# search for first non-empty sheet
-#		for n in range(self.wb.nsheets):
-#			if(self.wb.sheet_by_index(n).nrows):
-#				break
-#		if n==range(self.wb.nsheets):
-#			raise ReaderError('File empty')
-#
-#		self.datasheet = n
-#		sheet = self.wb.sheet_by_index(n)
-#		# search for first non-empty row
-#		for m in range(sheet.nrows):
-#			rv = sheet.row_values(m)
-#			for o in range(len(rv)):
-#				if rv[o] != '':
-#					break
-#			if o != len(rv)-1:
-#				break
-#		self.datarow = m
-#		self.datacol = o
-#		self.header = header
+	
+	def Header(self,header):
+		self.header = header
 
 	def getHeader(self):
 		if self.header:
-			ret = self.wb.sheet_by_index(self.datasheet).row_values(self.datarow,self.datacol)
+			ret = self.sheet.row_values(self.r1,self.c1)
 		else:
 			ret = []
 		return ret
 
-	def next(self):
-		if self.header:
-			headerskip = 1
-		else:
-			headerskip = 0
+	def setPivotTopLeft(self):
+		s = self.sheet
+		for r in range(s.nrows):
+			for c in range(s.ncols):
+				if s.cell(r,c).ctype != xlrd.XL_CELL_EMPTY:
+					self.r1 = r
+					self.c1 = c
+					return True
+		return False
 
-		if self.wb.sheet_by_index(self.datasheet).nrows > self.datarow+self.curr_record+headerskip:
-			ret = self.wb.sheet_by_index(self.datasheet).row_values(self.datarow+self.curr_record+headerskip,self.datacol)
-		else:
-			raise StopIteration
+	def setPivotKeyword(self,keyword):
+		s = self.sheet
+		for r in range(s.nrows):
+			for c in range(s.ncols):
+				if s.cell(r,c).ctype == keyword:
+					self.r1 = r
+					self.c1 = c
 
-		self.curr_record = self.curr_record + 1
-		return ret
+	def setPivot(self,row,col):
+		self.r1 = row
+		self.c1 = col
+		
+class ExcelFileReader:
+	class BookIterator:
+		def __init__(self,sheets):
+			self.curr_sheet = 0
+			self.sheets = sheets
+		
+		def next(self):
+			if len(self.sheets) > self.curr_sheet:
+				ret = self.sheets[self.curr_sheet]
+			else:
+				raise StopIteration
+
+			self.curr_sheet = self.curr_sheet + 1
+			return ret
+
+
+	def __init__(self):
+		self.filename = ""
+		self.wb = None
+		self.sheets = []
+
+	def load(self, filename, header = True):
+		self.wb = xlrd.open_workbook(filename) # book class
+		self.header = header
+		
+		if(self.wb != None):
+			self.filename = filename
+			for si in range(self.wb.nsheets):
+				try:
+					s = ExcelSheet(self.wb.sheet_by_index(si))
+					self.sheets.append(s)
+				except:
+					pass
 			
+	def __iter__(self):
+		return ExcelFileReader.BookIterator(self.sheets)
+
+	def __len__(self):
+		return len(self.sheets)
 	
 if __name__ == "__main__":
 	reader = ExcelFileReader()
 	reader.load('C:\\temp\\facebookupload\\book1.xls')
-	print reader.getHeader()
-	print len(reader)
-	for r in reader:
-		print r
+	for s in reader:
+		for r in s:
+			print r
+		print '\n'
+		for r in s:
+			print r
+		print '\n'
+	for s in reader:
+		for r in s:
+			print r
+		print '\n'
+		for r in s:
+			print r
+		print '\n'
 
 		
 
